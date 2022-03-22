8946f1755bddaa587623f0271a5a409128f61121
Estela Martinez <martinez26estela@gmail.con>
2022-03-22 11:22:31 -0600

    agregando ejercicio 48

---
+++ Ejercicios/Ejercicio48-Funciones-recursivas/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdlib.h> // srand, rand
+#include <time.h>
+
+int numeroSeceto = 7;
+void adivinarNumeroSecreto(int miNumero)
+{
+    if (miNumero == numeroSecreto)
+    {
+        cout << "Adivinaste!" << endl;
+    }
+    else
+    {
+        cout << "Intento fallido con" << miNumero << endl;
+        int otroNumero = 0;
+        cout << "Ingrese otro numero:";
+        cin >> otroNumero;
+        adivinarNumeroSecreto(otroNumero);
+    }
+
+
+
+}
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+
+{
+    adivinarNumeroSecreto(5);
+
+
+
+    return 0;
+}
