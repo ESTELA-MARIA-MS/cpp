6d8f277b2eb8e4b09d9395b022b8229af38f09bb
Estela Martinez <martinez26estela@gmail.con>
2022-03-23 09:36:19 -0600

    agregando ejercicio 53

---
+++ Ejercicios/Ejercicio53-Arreglos-Unidimensionales/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+int main(int argc, char** argv[]) 
+{
+	
+	int numero[5];  //declaracion del arreglo
+
+
+    // asignacion de valores a un arreglo unidimensional
+
+    numero[0] = 10;
+    numero[1] = 5;
+    numero[2] = 7;
+    numero[3] = 50;
+    numero[4] = 2;
+    
+    // lectura de los valores de un arreglo unidimensional
+    cout << numero[0] << endl;
+    cout << numero[1] << endl;
+    cout << numero[2] << endl;
+    cout << numero[3] << endl;
+    cout << numero[4] << endl;
+    
+    
+	return 0;
+}
