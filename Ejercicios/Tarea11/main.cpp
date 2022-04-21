33435ada21a88d508cc87f8cbafac87c401973d7
Estela Martinez <martinez26estela@gmail.con>
2022-04-20 21:08:23 -0500

    agregando tarea 11

--- Ejercicios/Tarea11/main.cpp
+++ Ejercicios/Tarea11/main.cpp
@@ -1,34 +1,46 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
-
-using namespace std;
-
-int main(int argc, char const *argv[]){
-	
-int a  [5];
-
-     //Inicializar cada elemento del arreglo con valores random
+#include <conio.h>
+ 
+using namespace std; 
+   
+int main()
+{
+    int N [5][5];
+    int suma=0; 
+   
+     //Inicializar cada elemento del arreglo con valores random//
      
-     srand (time(NULL));
-     
-     for (int i=0; i<=5; i++)
-	{
-			
-		a[i] = i *5;
-	
-    }
-    for (int i=0; i<=5; i++)
-	{
+    srand (time(NULL));
+    int random(); 
     
-	cout << a[i] << endl; 
+    for (int x = 0; x < 5; x++)
+    
+         for (int y = 0; y < 5; y++)
+        {
+         N [x][y] = random();
+         suma= suma + N [x][y];
+		}
+     //Genera numero entre 1 and 25://
+        for (int x = 0; x < 5; x++)
+        {
+         for (int y = 0; y < 5; y++)
+        {
+        	
+        printf  ("%2d ",N [x][y]);
 	
+       }
+        cout << endl;
     }
-    for (int i=0; i<=0; i++)
-	{
-		
-	   cout <<"Total de la suma:" ,a[i] + a[5];  
-    }
- 	
-	return 0;
+          
+    // Salida//
+    cout << endl;
+    cout <<"Total de la suma es:" << suma;
+  
+  return 0;
 }
+    int random()
+ {  
+return rand()% 10 + 1;
+ } 
